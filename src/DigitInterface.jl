1f3d6260dbca4aefe454c63e3853ff5dab28abe9
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2022-11-21 18:08:17 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/DigitInterface.jl
@@ -0,0 +1,5 @@
+module DigitInterface
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DigitInterface
+using Test
+
+@testset "DigitInterface.jl" begin
+    # Write your tests here.
+end
